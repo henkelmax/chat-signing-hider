8ae17bf29eda0b73427e59002ef999bbd8d4f50c
Max Henkel <max@maxhenkel.de>
2024-02-18 10:33:49 +0100

    Fix compatibility with other mods Closes #2
    (cherry picked from commit 18af9635800377af16c1fe242b8e17ce44868c48)

--- src/main/java/de/maxhenkel/chatsigninghider/mixin/ClientPacketListenerMixin.java
+++ src/main/java/de/maxhenkel/chatsigninghider/mixin/ClientPacketListenerMixin.java
@@ -1,7 +1,8 @@
 package de.maxhenkel.chatsigninghider.mixin;
 
+import net.minecraft.client.gui.components.toasts.Toast;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.network.protocol.game.ClientboundServerDataPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -9,9 +10,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ClientPacketListener.class)
 public abstract class ClientPacketListenerMixin {
 
-    @Redirect(method = "handleServerData", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ClientboundServerDataPacket;enforcesSecureChat()Z", ordinal = 1))
-    private boolean enforcesSecureChat(ClientboundServerDataPacket packet) {
-        return true;
+    @Redirect(method = "handleServerData", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/toasts/ToastComponent;addToast(Lnet/minecraft/client/gui/components/toasts/Toast;)V"))
+    private void enforcesSecureChat(ToastComponent instance, Toast toast) {
+        // Just don't show the toast
     }
 
 }
