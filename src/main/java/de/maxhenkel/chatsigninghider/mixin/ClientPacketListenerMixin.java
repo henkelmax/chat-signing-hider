42008f301aed310813f9c34ece08c6b8d3b1b4ed
Max Henkel <max@maxhenkel.de>
2024-10-17 18:37:47 +0200

    Update to 1.21.2-rc1

--- src/main/java/de/maxhenkel/chatsigninghider/mixin/ClientPacketListenerMixin.java
+++ src/main/java/de/maxhenkel/chatsigninghider/mixin/ClientPacketListenerMixin.java
@@ -1,7 +1,7 @@
 package de.maxhenkel.chatsigninghider.mixin;
 
 import net.minecraft.client.gui.components.toasts.Toast;
-import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.gui.components.toasts.ToastManager;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ClientPacketListener.class)
 public abstract class ClientPacketListenerMixin {
 
-    @Redirect(method = "handleLogin", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/toasts/ToastComponent;addToast(Lnet/minecraft/client/gui/components/toasts/Toast;)V"))
-    private void addToast(ToastComponent instance, Toast toast) {
+    @Redirect(method = "handleLogin", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/toasts/ToastManager;addToast(Lnet/minecraft/client/gui/components/toasts/Toast;)V"))
+    private void addToast(ToastManager instance, Toast toast) {
         // Just don't show the toast
     }
 
