99abc29d64d8b8128f07d4606cd2e5bdbd0c6010
Max Henkel <max@maxhenkel.de>
2026-03-15 07:48:22 +0100

    Update to 26.1-pre1

--- src/main/java/de/maxhenkel/chatsigninghider/mixin/ChatComponentMixin.java
+++ src/main/java/de/maxhenkel/chatsigninghider/mixin/ChatComponentMixin.java
@@ -2,17 +2,16 @@ package de.maxhenkel.chatsigninghider.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.GuiMessage;
-import net.minecraft.client.GuiMessageTag;
-import net.minecraft.client.gui.components.ChatComponent;
+import net.minecraft.client.multiplayer.chat.GuiMessage;
+import net.minecraft.client.multiplayer.chat.GuiMessageTag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(ChatComponent.class)
+@Mixin(targets = "net/minecraft/client/gui/components/ChatComponent$1")
 public abstract class ChatComponentMixin {
 
-    @WrapOperation(method = "addMessageToDisplayQueue", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/GuiMessage;tag()Lnet/minecraft/client/GuiMessageTag;"))
-    private GuiMessageTag addMessageToDisplayQueue(GuiMessage instance, Operation<GuiMessageTag> original) {
+    @WrapOperation(method = "accept", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/chat/GuiMessage$Line;tag()Lnet/minecraft/client/multiplayer/chat/GuiMessageTag;"))
+    private GuiMessageTag accept(GuiMessage.Line instance, Operation<GuiMessageTag> original) {
         return null;
     }
 
